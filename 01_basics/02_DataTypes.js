b5f0de5effc84526de0a5469767efcd58cf8bd31
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2025-03-08 23:19:52 +0530

    Add files via upload

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"Use strict"; // treat all JS code as newer version 
+
+// alert("Hello") // we are using nodejs, not browser
+
+console.log(3 + 3);
+console.log("Ravi"); // code readability should be high
+
+let name = "Ravi"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null);  // object 
+console.log(typeof undefined); // undefined
