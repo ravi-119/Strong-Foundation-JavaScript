2144d85c7903fa30b5fbad6a2703c484b6055e7f
ravi-119 <ry277015@gmail.com>
2025-03-23 13:14:11 +0530

    Destructuring part is added

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -55,3 +55,35 @@ console.log(Object.entries(tinderUser));
 console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
+
+
+
+
+// Destructuring Objects
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+
